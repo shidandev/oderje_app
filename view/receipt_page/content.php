46e199ce84dad53cd3676494765ed78f0dca92cc
shidandev <shidan95.dev@gmail.com>
2019-12-10 01:47:00 +0800

    10/12/2019 Receipt

---
+++ view/receipt_page/content.php
@@ -0,0 +1,33 @@
+<div class="container-fluid">
+	
+	<div class="row">
+		<div class="col-md-2 col-sm-1 col-1"></div>
+		<div class="col-md-8 col-sm-10 col-10 border border-rounded">
+			
+			<div class="col-md-12 col-sm-12 col-12 text-center text-outline-custom1 text-warning h5 p-0">Receipt</div>
+			<div class="row p-0">
+				<div class="col-md-4 col-sm-4 col-4  p-0 text-center">
+					<img src="../img/oderje-logo.png"  style="width:50%;">
+				</div>
+				<div class="col-md-8 col-sm-8 col-8  p-0 ">
+					<table class="">
+						<tr><td class="p-0 text-right " width="54%"><small class="font-weight-bold">Bill Ref</small></td><td width="1%" class="p-0 font-weight-bold">:</td><td width="45%" class="p-0 text-right font-weight-bold"><span>OJ99999999</span></td></tr>
+						<tr><td class="p-0 text-right"><small class="font-weight-bold">Bill Date</small></td><td class="p-0 font-weight-bold">:</td><td class="p-0 text-right font-weight-bold"><span> 11/12/2019 </span></td></tr>
+					</table>
+					<!-- <label class="col-md-12 col-sm-12 col-12 text-right p-0 m-0 font-weight-bold"><small>Bill Ref</small> : <span>OJ99999999</span></label>
+					<label class="col-md-12 col-sm-12 col-12 text-right p-0 m-0 font-weight-bold"><small>Bill Date</small> : <span> 11/12/2019 </span></label> -->
+				</div>
+			</div>			
+			<div class="row mx-auto mt-3">
+				<table class="col-md-12 col-sm-12 col-12">
+					<tr><td width="50%" class="text-center">Amount</td><td width="1%">:</td><td class="text-center" width="49%">RM 5.05</td></tr>
+					<tr><td width="50%" class="text-center bg-info">Customer</td><td width="1%">:</td><td class="text-center" width="49%">Mohamad Syahiddan bin Mohd Nasir</td></tr>
+				</table>
+			</div>
+			<div class="row mx-auto mt-3">
+				<button>print</button>
+			</div>
+		</div>
+		<div class="col-md-2 col-sm-1 col-1"></div>
+	</div>
+</div>
