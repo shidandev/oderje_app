5afd6be0e10abd1b3fb7facecb4345debc614a6f
Syahiddan Laptop <shidan95.dev@gmail.com>
2020-01-13 17:52:09 +0800

    dependencies change path

--- head/head-child-child.php
+++ head/head-child-child.php
@@ -2,26 +2,26 @@
 	<meta charset="utf-8">
 	 <link rel="icon" href="../../img/oderje-favicon.png?">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/css/bootstrap.min.css">
-    <link href="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/css/text.css" rel="stylesheet">
-    <link href="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/css/box.css" rel="stylesheet">
-    <link href="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/css/hide-scroll.css" rel="stylesheet">
-    <link href="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/css/background.css" rel="stylesheet">
-    <link href="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/css/btn.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link href="../../css/text.css" rel="stylesheet">
+    <link href="../../css/box.css" rel="stylesheet">
+    <link href="../../css/hide-scroll.css" rel="stylesheet">
+    <link href="../../css/background.css" rel="stylesheet">
+    <link href="../../css/btn.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
     <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
 
     <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
    
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/js/jquery.min.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/js/bootstrap.min.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/js/popper.min.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/js/number-input-spinner.js"></script>
+    <script src="../../js/jquery.min.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/popper.min.js"></script>
+    <script src="../../js/number-input-spinner.js"></script>
   
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/controller/Session.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/controller/encodedecode.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/controller/PARAM.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/modal/Product.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/modal/ProductBasket.js"></script>
-    <script src="<?php $_SERVER['DOCUMENT_ROOT']?>/www.oderje.com/modal/BasketByMerchant.js"></script>
+    <script src="../../controller/Session.js"></script>
+    <script src="../../controller/encodedecode.js"></script>
+    <script src="../../controller/PARAM.js"></script>
+    <script src="../../modal/Product.js"></script>
+    <script src="../../modal/ProductBasket.js"></script>
+    <script src="../../modal/BasketByMerchant.js"></script>
 </head>
--- payment/pos/index.php
+++ payment/pos/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
-	<?php include_once($_SERVER['DOCUMENT_ROOT']."/www.oderje.com/head/head-child-child.php");?>
+	<?php include_once("../../head/head-child-child.php");?>
 <body>
-	<?php include_once($_SERVER['DOCUMENT_ROOT']."/www.oderje.com/controller/user.php");?>
+	<?php include_once("../../controller/user.php");?>
 
-	<?php include_once($_SERVER['DOCUMENT_ROOT']."/www.oderje.com/view/common/navbar-child.php");?>
+	<?php include_once("../../view/common/navbar-child.php");?>
 	
 </body>
 </html>
